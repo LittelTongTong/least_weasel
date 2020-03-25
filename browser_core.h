e9779e176554a175de901422102134ea8812cb5a
黄骏东 <huangjundong@huangjundongs-MacBook-Pro.local>
2020-03-25 09:39:14 +0800

    ture

---
+++ browser_core.h
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#define M_N_W 100 
+//website socket information 
+struct w_s_d
+{
+    char * website;
+    struct addrinfo * website_addrinfo;
+};
+//convert  addrinfo  to readable ip and print 
+int AI_2_RI(struct addrinfo* info,char *string )
+{ 
+    if ((info->ai_family)==AF_INET)
+    {
+        struct sockaddr_in * ip4_socket_addr=(struct sockaddr_in * )(info->ai_addr);
+        inet_ntop(AF_INET,&(ip4_socket_addr->sin_addr),string,ip4_socket_addr->sin_len);
+    }
+    if ((info->ai_family)==AF_INET6)
+    {
+        struct sockaddr_in6 * ip6_socket_addr=(struct sockaddr_in6 * )(info->ai_addr);
+        inet_ntop(AF_INET6,&(ip6_socket_addr->sin6_addr),string,ip6_socket_addr->sin6_len);
+    }
+    printf("canonneme:%s 's ip:%s\n",info->ai_canonname,string);
+    return 0;
+}
+//DNS服务
+int DNS(char *website ,struct w_s_d * web_socket_data)//网址
+{
+    struct addrinfo hint;
+    struct addrinfo *res0;
+    memset(&hint,0,sizeof(struct addrinfo));
+    hint.ai_family=AF_UNSPEC;
+    hint.ai_socktype=SOCK_STREAM;
+    P_E(getaddrinfo(website,NULL,&hint,&res0),"getaddinfo");
+    printf("|%s| done\n",website);
+    web_socket_data->website=website;
+    web_socket_data->website_addrinfo=res0;
+    return 0;
+}
+//连接到网站服务器
+int C2Ws(struct w_s_d * web_socket_data)// C2Ws(websit server sock info, send buff,number of  buff to send, receive buff,number of  buff to receive )
+{
+    int sfd;
+    int d = web_socket_data->website_addrinfo->ai_family;//socket domain 
+    int t = web_socket_data->website_addrinfo->ai_socktype;//socket type 
+    int p = web_socket_data->website_addrinfo->ai_protocol;//socket protocol 
+    size_t len =web_socket_data->website_addrinfo->ai_addr->sa_len;
+    char sbuff[BUFSIZ]="GET /index.html HTTP/1.1\nHost:sse.com.cn\r\n";
+    char rbuff[BUFSIZ];
+    memset(&rbuff,0,BUFSIZ);
+    if ((sfd = socket(d,t,p)))
+    {
+        //set port number F!U!C!K! 否则会报错 “connect(): Can't assign requested address ” ！！！！！！
+        switch (d)
+        {
+        case AF_INET:
+            ((struct sockaddr_in*)(web_socket_data->website_addrinfo->ai_addr))->sin_port=htons(80);
+            break;
+        case AF_INET6:
+            ((struct sockaddr_in6*)(web_socket_data->website_addrinfo->ai_addr))->sin6_port=htons(80);
+        default:
+            break;
+        }
+        printf("connect to |%s|\n",web_socket_data->website);
+        if ((connect(sfd, web_socket_data->website_addrinfo->ai_addr,len)==-1))
+        {
+            perror("connect()");
+        }
+        else
+        {
+            printf("website server connect successfully\n");
+            if (( send(sfd,sbuff,sizeof(sbuff),0)==-1))
+            {
+                perror("send()");
+            }
+            if (( recv(sfd,rbuff,sizeof(rbuff),0)==-1))
+            {
+                perror("send()");
+            }
+            printf("%s",rbuff);
+            
+        }
+        
+    }
+    
+    
+    else
+    {
+        perror("socket()");
+    }
+    close(sfd);
+    return 0 ; 
+}
+
